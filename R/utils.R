c670e28475e4d7c4a9cf6fb706d03e6f2e6ceaf9
Yulin Lyu <lvyulin@pku.edu.cn>
2023-11-10 03:31:29 +0800

    update util

--- R/utils.R
+++ R/utils.R
@@ -15,3 +15,13 @@
 getMatColor <- function(pal = "blue", num = 3L) {
   pal_material(palette = pal, n = 2 * num + 1)(2 * num + 1)[2 * 1:num]
 }
+
+uniqueID <- function(name, ID) {
+  stopifnot("ID must be unique!" = all(!duplicated(ID)))
+  stopifnot("ID and name must have the same length!" = length(name) == length(ID))
+  
+  dupName <- unique(name[duplicated(name)])
+  name[name %in% dupName] <- paste0(name[name %in% dupName], "_", ID[name %in% dupName])
+  name
+}
+
